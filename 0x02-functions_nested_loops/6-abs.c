1c0a42aa55ebd4b6cb1cfb30b7bb47f63468f1a6
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-03-21 16:36:27 -0700

    Write a function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - print the absolute value of n
+ *
+ * @n: type int
+ *
+ * Return: abs of n
+ */
+int _abs(int n)
+{
+	int a = 0;
+
+	a = abs(n);
+	return (a);
+}
