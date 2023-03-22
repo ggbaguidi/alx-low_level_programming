5e5e2a3ea913e27626317691543dd95eed1384c9
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-03-21 19:17:28 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,23 +14,18 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
-			printf("%d", i);
-			if (i < 98)
-			{
-				printf(", ");
-			}
+			printf("%d, ", i);
 		}
+		printf("%d\n", i);
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			printf("%d", i);
-			if (i > 98)
-				printf(", ");
+			printf("%d, ", i);
 		}
+		printf("%d\n", i);
 	}
-	_putchar('\n');
 }
