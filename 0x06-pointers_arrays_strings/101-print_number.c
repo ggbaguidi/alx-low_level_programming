cfaf02804130293abcf961129a580c847cd8e4af
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-03-29 16:35:08 -0700

    Write a function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,32 +1,17 @@
 #include "main.h"
 
 /**
- * base10 - power in 10 base
- * @n: an exponent
- * Return: returns 10 to power exponent
- */
-int base10(int n)
-{
-	int base = 10;
-
-	while (n > 0)
-	{
-		base *= 10;
-		n--;
-	}
-	return (base);
-}
-
-/**
- * print_number - prints integers enters as parameters using putchar
- * @n: integer to print
- * Return: void
+ * print_number - print number
+ *
+ * @n: type int
+ *
+ *Guy A GBAGUIDI
+ *
+ * Return: None
  */
 void print_number(int n)
 {
-	int power;
-
-	power = base10(8);
+	unsigned int x;
 
 	if (n < 0)
 	{
@@ -34,19 +19,10 @@ void print_number(int n)
 		n *= -1;
 	}
 
-	if (n == 0)
-		_putchar('0');
+	x = n;
 
-	else
-	{
-		while (n / power == 0)
-			power /= 10;
+	if (x / 10)
+		print_number(x / 10);
 
-		while (power >= 1)
-		{
-			_putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
-		}
-	}
+	_putchar(x % 10 + '0');
 }
