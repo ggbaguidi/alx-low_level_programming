cc7f8271c74ae45a7ad1077146536091ed38e478
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-03-29 14:23:49 -0700

    Write a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concartenate two string
+ *
+ * @dest: type char pointer
+ *
+ * @src: type char pointer
+ *
+ * Owned by Guy A GBAGUIDI
+ *
+ * Return: type char pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
