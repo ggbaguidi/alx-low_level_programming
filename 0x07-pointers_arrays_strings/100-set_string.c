f21507af342262a43cf3e4403a99e67db4d9d0b8
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-04-03 19:00:50 -0700

    Write a function that sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_string - set string function
+ *
+ * @s: type pointer to char pointer
+ *
+ * @to: type char pointer
+ *
+ * Owned by Guy A GBAGUIDI
+ *
+ * Return: None
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
