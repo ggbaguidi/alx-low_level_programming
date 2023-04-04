1f745ae99e05d45cf0761c335fdeab408431ec88
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-04-03 18:45:05 -0700

    Write a function that locates a substring.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring
+ *
+ * @haystack: a type char pointer
+ *
+ * @needle: a type char pointer
+ *
+ * Return: a substring as type a char pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
