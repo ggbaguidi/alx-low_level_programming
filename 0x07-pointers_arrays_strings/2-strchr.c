d26e845bb59fc7d2e1993c1a956f3224645696b6
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-04-03 18:30:54 -0700

    Write a function that locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strchr - search character in string
+ *
+ * @s: type char pointer
+ *
+ * @c: type char
+ *
+ * Owned by Guy A GBAGUIDI
+ *
+ * Return: char pointer
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
