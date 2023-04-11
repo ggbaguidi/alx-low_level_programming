a5d807dfbc458e504ff53b18543fe9376c1f54db
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-04-11 13:59:12 -0700

    Write a function that creates an array of chars, and initializes it with a specific char.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_array - ceate a char array
+ *
+ * @size: unsigned int type
+ *
+ * @c: type char initial char
+ *
+ *Owned by Guy A GBAGUIDI
+ *
+ * Return: a char pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
