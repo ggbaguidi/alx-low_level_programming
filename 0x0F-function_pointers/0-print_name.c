9c31525d2b41ba78919236f6355c20dc95623855
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-04-19 13:07:19 -0700

    Write a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints a name
+ *
+ * @name: a char pointer
+ * @f: a function pointer which take a char pointer and return void
+ * Owned by Guy A GBAGUIDI
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
