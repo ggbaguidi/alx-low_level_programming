e62c287f8fb8a095a5cac9eb7bb48dcba106335a
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-04-20 18:13:01 -0700

    update commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,20 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
 	unsigned int i;
+	va_list ap;
 
 	va_start(ap, n);
-	for (i = 0; i < n - 1; i++)
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (separator != NULL)
+		if (i < n - 1)
 			printf("%s", separator);
-		printf(" ");
 	}
-	printf("%d\n", va_arg(ap, int));
-	va_end(ap)
+	printf("\n");
+	va_end(ap);
 }
