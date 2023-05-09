9a02f067b54c2db4bba3febb529daa98849abc64
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-05-08 17:03:45 -0700

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,14 @@ int create_file(const char *filename, char *text_content)
 	mode_t filePerms = S_IRUSR | S_IWUSR;
 	int openFlags = O_CREAT | O_RDWR  | O_TRUNC;
 	ssize_t outputFd, numwr;
+	size_t size = 0;
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content != NULL)
+		size = strlen(text_content);
 	outputFd = open(filename, openFlags, filePerms);
-	numwr = write(outputFd, text_content, strlen(text_content));
+	numwr = write(outputFd, text_content, size);
 	if (numwr == -1 || outputFd == -1)
 		return (-1);
 	close(outputFd);
